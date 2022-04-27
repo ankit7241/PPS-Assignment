97177975a2d8db8aada7a9da1f871106f8d41964
Ankit Choudhary <sign.ankit14@gmail.com>
2022-04-28 00:07:10 +0530

    Update calculator.c
    Last commit failed so this time did some changes

--- calculator.c
+++ calculator.c
@@ -5,7 +5,7 @@ int main()
     int choice,num1,num2,num;
     printf("Welcome to my mini project!!\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Square\n6.Cube\n7.Root\nEnter your choice : ");
     scanf("%d",&choice);
-    if(choice >=1  && choice<=4)
+    if(choice >=1 && choice<=4)
     {
         printf("\nEnter 2 Numbers:\n");
         scanf("%d%d",&num1,&num2);
